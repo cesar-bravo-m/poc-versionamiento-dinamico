6369d7bf040426f1b70efe22c3ecffc278ea21f1
César Bravo <cbravo@bcyt.cl>
2025-07-22 00:35:41 -0400

    Checkpoint

--- version-backend/SignalRWebpack/Program.cs
+++ version-backend/SignalRWebpack/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-            .WithOrigins("http://localhost:3001", "http://localhost:5018", "http://localhost:5173")
+            .WithOrigins("http://localhost:3001", "http://localhost:5018", "http://localhost:5173", "http://localhost:5174")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
